e8510334951cbde6bad54371526d9538f0dd5178
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-19 16:30:22 +0300

    Arkanoid

--- Arkanoid/Arkanoid.cpp
+++ Arkanoid/Arkanoid.cpp
@@ -1,20 +1,97 @@
 // Arkanoid.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace sf;
+
+bool isCollide(Sprite s1, Sprite s2);
+
+int main() {
+	srand(time(0));
+
+	RenderWindow app(VideoMode(520, 450), "Arkanoid Game!");
+	app.setFramerateLimit(60);
+
+	Texture t1, t2, t3, t4;
+	t1.loadFromFile("images/block01.png");
+	t2.loadFromFile("images/background.jpg");
+	t3.loadFromFile("images/ball.png");
+	t4.loadFromFile("images/paddle.png");
+
+	Sprite sBackground(t2), sBall(t3), sPaddle(t4);
+	sPaddle.setPosition(300, 440);
+	sBall.setPosition(300, 300);
+
+	Sprite block[1000];
+
+	int n = 0;
+
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++){
+			block[n].setTexture(t1);
+			block[n].setPosition(i * 43, j * 20);
+			n++;
+		}
+
+	float dx = 6, dy = 5;
+
+	while (app.isOpen()) {
+		Event e;
+		while (app.pollEvent(e)) {
+			if (e.type == Event::Closed)
+				app.close();
+		}
+
+		sBall.move(dx,0);
+
+		for (int i = 0; i < n; i++) {
+			if (isCollide(sBall, block[i])) {
+				block[i].setPosition(-100, 0);
+				dx =- dx;
+			}
+		}
+
+		sBall.move(0, dy);
+
+		for (int i = 0; i < n; i++) {
+			if (isCollide(sBall, block[i])) {
+				block[i].setPosition(-100, 0);
+				dy =- dy;
+			}
+		}
+
+		Vector2f b = sBall.getPosition();
+		if (b.x < 0 || b.x > 520)	dx =- dx;
+		if (b.y < 0 || b.y > 450)	dy =- dy;
+
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+			app.close();
+
+		if (Keyboard::isKeyPressed(Keyboard::D))
+			sPaddle.move(6, 0);
+		if (Keyboard::isKeyPressed(Keyboard::A))
+			sPaddle.move(-6, 0);
+
+		if (isCollide(sPaddle, sBall))
+			dy = -(rand() % 5 + 2);
+
+		app.clear();
+		app.draw(sBackground);
+		app.draw(sBall);
+		app.draw(sPaddle);
+
+		for (int i = 0; i < n; i++)
+			app.draw(block[i]);
+
+		app.display();
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool isCollide(Sprite s1, Sprite s2) {
+	return s1.getGlobalBounds().intersects(s2.getGlobalBounds());
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
